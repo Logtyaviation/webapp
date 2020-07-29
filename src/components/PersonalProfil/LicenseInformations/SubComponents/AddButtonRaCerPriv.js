832c55d412ebb78779c000d0f333d0aa12546955
Raphael Arce <raphael.arce@campus.tu-berlin.de>
2020-07-29 22:41:40 +0200

    added more details as comments

--- src/components/PersonalProfil/LicenseInformations/SubComponents/AddButtonRaCerPriv.js
+++ src/components/PersonalProfil/LicenseInformations/SubComponents/AddButtonRaCerPriv.js
@@ -15,7 +15,7 @@ class AddButtonRaCerPriv extends Component {
         // chaque fois que le button add + eventKey correspondant sera cliqué on va ajouter
         // cet eventKey dans l'array selectedKeys. Sauf qu'on peut pas directement ajouter
         // un element à un array dans le state de react, du coup je le recupere dans 
-        // une constante locale, j'y ajoute le nouvel eventKey, puis j'écrase l'ancien array
+        // une constante locale, on y ajoute le nouvel eventKey, puis j'écrase l'ancien array
         //it dans le state avec la constante locale
         const newSelectedKeys = this.state.selectedKeys
         newSelectedKeys.push(eventKey)
@@ -36,8 +36,14 @@ class AddButtonRaCerPriv extends Component {
          *  return <Component prop={element}/>
          * })
          * mappedArray sera egal à [<Component prop={a}/>, <Component prop={b}/>, <Component prop={c}/>]
+         * 
          * Pour plus d'infos: 
          * https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+         * 
+         * La façon de noter array.map(element => element*2) par exemple est une abreviation pour:
+         * array.map(function callback(element) {
+         *  return element*2
+         * })
          */
         return this.state.selectedKeys.map((selectedKey) => {
             return <NewLineRaCerPriv theKey={selectedKey}/>
